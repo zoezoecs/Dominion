2e143979d96059a252381f31d6abe05a85007512
Zoe <zoe.subscription@yahoo.com>
2026-04-18 02:13:38 +1000

    Fixing more silly Stacks bugs

--- src/Interpreters/Stacks.hs
+++ src/Interpreters/Stacks.hs
@@ -20,7 +20,7 @@ unsafeLookup :: Ord k => k -> Map k c -> c
 unsafeLookup l = fromJust . Map.lookup l
 
 refill :: (Member Stacks r, Foldable t) => PileConfig t -> Position -> Sem r ()
-refill (PileConfig fillFrom shuffleOnFill) l = do
+refill (PileConfig fillFrom shuffleOnFill) l = trace "wah" $ do
     case Map.lookup l fillFrom of
       Nothing -> return ()
       Just l' -> do
@@ -45,10 +45,10 @@ interpStacks cfg = interpret $ \case
       DrawTo l1 l2 -> do
         cardMap <- get @(Map Position [Card])
         let stack1 = unsafeLookup l1 cardMap
-        let stack2 = unsafeLookup l2 cardMap
         when (null stack1) $ interpStacks cfg $ refill cfg l1
         cardMap <- get @(Map Position [Card])
         let stack1 = unsafeLookup l1 cardMap
+        let stack2 = unsafeLookup l2 cardMap
         case stack1 of
             []                -> return Nothing
             (x:xs)            -> do
@@ -58,7 +58,7 @@ interpStacks cfg = interpret $ \case
         cardMap <- get @(Map Position [Card])
         let notACard = Map.null $ Map.filter (elem card) cardMap
         when notACard undefined
-        let newMap = fmap (filter (card ==)) cardMap -- undefined if this doesn't change it?
+        let newMap = fmap (filter (card /=)) cardMap -- undefined if this doesn't change it?
         let stack1 = unsafeLookup loc newMap
         void $ put $ Map.insert loc (card:stack1) newMap
 
